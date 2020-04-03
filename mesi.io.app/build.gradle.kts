b76ac32361b506e0d51025efa56967ce1f8bbdec
mesinger <uintptr96@gmail.com>
2020-04-03 16:15:53 +0200

    Disable non secured requests

--- mesi.io.app/build.gradle.kts
+++ mesi.io.app/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-//	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
