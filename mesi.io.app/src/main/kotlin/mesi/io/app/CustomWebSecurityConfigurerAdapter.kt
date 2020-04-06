edf4a14df7c1cbe2f6b63697b5c5902e536c652c
mesinger <uintptr96@gmail.com>
2020-04-06 23:34:26 +0200

    Remove spring boot security as it uses nginx reverse proxy on aws

--- mesi.io.app/src/main/kotlin/mesi/io/app/CustomWebSecurityConfigurerAdapter.kt
+++ mesi.io.app/src/main/kotlin/mesi/io/app/CustomWebSecurityConfigurerAdapter.kt
@@ -1,12 +1,12 @@
 package mesi.io.app
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-
-@EnableWebSecurity
-class CustomWebSecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
-        http.requiresChannel().anyRequest().requiresSecure()
-    }
-}
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+//
+//@EnableWebSecurity
+//class CustomWebSecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
+//    override fun configure(http: HttpSecurity) {
+//        http.requiresChannel().anyRequest().requiresSecure()
+//    }
+//}
--- mesi.io.app/src/main/kotlin/mesi/io/app/ServerConfiguration.kt
+++ mesi.io.app/src/main/kotlin/mesi/io/app/ServerConfiguration.kt
@@ -1,40 +1,40 @@
 package mesi.io.app
 
-import org.apache.catalina.Context
-import org.apache.catalina.connector.Connector
-import org.apache.tomcat.util.descriptor.web.SecurityCollection
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-
-@Configuration
-class ServerConfiguration {
-
-    @Bean
-    fun servletWebServerFactory() : ServletWebServerFactory {
-        val tomcat: TomcatServletWebServerFactory = object : TomcatServletWebServerFactory() {
-            override fun postProcessContext(context: Context) {
-                val securityConstraint = SecurityConstraint()
-                securityConstraint.userConstraint = "CONFIDENTIAL"
-                val collection = SecurityCollection()
-                collection.addPattern("/*")
-                securityConstraint.addCollection(collection)
-                context.addConstraint(securityConstraint)
-            }
-        }
-        tomcat.addAdditionalTomcatConnectors(getHttpConnector())
-        return tomcat
-    }
-
-    private fun getHttpConnector() : Connector {
-        val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
-        connector.scheme = "http"
-        connector.port = 8080
-        connector.secure = false
-        connector.redirectPort = 443
-        return connector
-    }
-}
+//import org.apache.catalina.Context
+//import org.apache.catalina.connector.Connector
+//import org.apache.tomcat.util.descriptor.web.SecurityCollection
+//import org.apache.tomcat.util.descriptor.web.SecurityConstraint
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+//import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//
+//
+//@Configuration
+//class ServerConfiguration {
+//
+//    @Bean
+//    fun servletWebServerFactory() : ServletWebServerFactory {
+//        val tomcat: TomcatServletWebServerFactory = object : TomcatServletWebServerFactory() {
+//            override fun postProcessContext(context: Context) {
+//                val securityConstraint = SecurityConstraint()
+//                securityConstraint.userConstraint = "CONFIDENTIAL"
+//                val collection = SecurityCollection()
+//                collection.addPattern("/*")
+//                securityConstraint.addCollection(collection)
+//                context.addConstraint(securityConstraint)
+//            }
+//        }
+//        tomcat.addAdditionalTomcatConnectors(getHttpConnector())
+//        return tomcat
+//    }
+//
+//    private fun getHttpConnector() : Connector {
+//        val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+//        connector.scheme = "http"
+//        connector.port = 8080
+//        connector.secure = false
+//        connector.redirectPort = 443
+//        return connector
+//    }
+//}
